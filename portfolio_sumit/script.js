641f2d790d7a598001e88d301166ea97aa617ab6
kumarsumitjais <155108011+kumarsumitjais@users.noreply.github.com>
2025-10-18 22:15:37 +0530

    Delete portfolio_sumit directory

--- portfolio_sumit/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.circular-progress').forEach(progress => {
-    const percentage = progress.getAttribute('data-percentage'); // Get the percentage value
-    const fill = progress.querySelector('.mask.full .fill'); // Select the fill element
-    const rotation = (360 * percentage) / 100; // Calculate the rotation angle
-    fill.style.transform = `rotate(${rotation}deg)`; // Apply the rotation
-});
